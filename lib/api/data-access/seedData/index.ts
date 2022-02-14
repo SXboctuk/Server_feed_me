65abd5f7c3b48c25a72ec8b4ed2e782b931745d3
Maksim Lisitski <stargate.xboctuk@gmail.com>
2022-02-14 12:44:41 +0300

    base seed

--- lib/api/data-access/seedData/index.ts
+++ lib/api/data-access/seedData/index.ts
@@ -1,6 +1,5 @@
 import { cookbooks } from "./cookbook";
 import { recepies } from "./recepies";
-import { recepiesInCookbooks } from "./recepiesInCookbooks";
 import { users } from "./users";
 
 export const seedData = { cookbooks, recepies, users };
