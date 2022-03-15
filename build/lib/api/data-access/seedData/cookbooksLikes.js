a8400586d39e9d28316d3992c58b438a4856d7cb
SXboctuk <40503787+SXboctuk@users.noreply.github.com>
2022-03-15 20:23:55 +0300

    Delete build directory

--- build/lib/api/data-access/seedData/cookbooksLikes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.cookbooksLikes = void 0;
-exports.cookbooksLikes = [
-    {
-        RecepieId: "",
-        UserId: "",
-    },
-];
--- build/lib/helpers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validator = exports.tokenUtils = exports.commonUtils = void 0;
-const token_util_1 = require("./utils/token.util");
-Object.defineProperty(exports, "tokenUtils", { enumerable: true, get: function () { return token_util_1.tokenUtils; } });
-const common_1 = require("./utils/common");
-Object.defineProperty(exports, "commonUtils", { enumerable: true, get: function () { return common_1.commonUtils; } });
-const validators_1 = require("./validators");
-Object.defineProperty(exports, "validator", { enumerable: true, get: function () { return validators_1.validator; } });
