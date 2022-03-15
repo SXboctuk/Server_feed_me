6e1dbc7e11510fe8c3dc938737fa71af19c77be8
Maksim Lisitski <stargate.xboctuk@gmail.com>
2022-03-15 20:08:46 +0300

    heroku t1

--- build/lib/constants/config/server.config.js
+++ build/lib/constants/config/server.config.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serverConfig = void 0;
 exports.serverConfig = {
-    port: Number(process.env.SERVER_PORT) || 3000,
+    port: Number(process.env.SERVER_PORT) || 8080,
     hostName: process.env.HOST_NAME || "localhost",
 };
--- lib/constants/config/server.config.ts
+++ lib/constants/config/server.config.ts
@@ -1,4 +1,4 @@
 export const serverConfig = {
-    port: Number(process.env.SERVER_PORT) || 3000,
-    hostName: process.env.HOST_NAME || "localhost",
+  port: Number(process.env.SERVER_PORT) || 8080,
+  hostName: process.env.HOST_NAME || "localhost",
 };
