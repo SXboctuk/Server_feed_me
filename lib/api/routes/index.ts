0c912f655424c28bee31b1159d0de3fd7f56c071
Maksim Lisitski <stargate.xboctuk@gmail.com>
2022-03-17 15:57:26 +0300

    db

--- lib/api/routes/index.ts
+++ lib/api/routes/index.ts
@@ -1,16 +1,16 @@
-import { Router } from "express";
-import authRoute from "./auth.routes";
-import cookbookRoute from "./cookbook.routes";
-import recepieRoute from "./recepie.routes";
-import userRoute from "./user.routes";
+import { Router } from 'express';
+import authRoute from './auth.routes';
+import cookbookRoute from './cookbook.routes';
+import recepieRoute from './recepie.routes';
+import userRoute from './user.routes';
 
 const mainRoute = Router();
 
-mainRoute.use("/auth", authRoute);
-mainRoute.use("/user", userRoute);
-mainRoute.use("/recepie", recepieRoute);
-mainRoute.use("/cookbook", cookbookRoute);
-mainRoute.use("/", (req, res) => {
-    res.end(`<h1> Hello its me</h1>`);
+mainRoute.use('/auth', authRoute);
+mainRoute.use('/user', userRoute);
+mainRoute.use('/recepie', recepieRoute);
+mainRoute.use('/cookbook', cookbookRoute);
+mainRoute.use('/', (req, res) => {
+    res.end(`<h1> Hello its me</h1> `);
 });
 export default mainRoute;
