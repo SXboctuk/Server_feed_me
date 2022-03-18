a899fe64ddfb2451d6a827f5d8a18b6bb859b281
Maksim Lisitski <stargate.xboctuk@gmail.com>
2022-03-18 17:36:39 +0300

    fix

--- lib/middleware/auth.middleware.ts
+++ lib/middleware/auth.middleware.ts
@@ -10,7 +10,7 @@ const verifyAuthToken = async (
         const token = req.cookies['jwt'];
 
         if (!token) {
-            res.status(403).json({ message: 'auth error' }).end();
+            return res.status(403).json({ message: 'auth error' }).end();
         }
 
         const userPayload = tokenUtils.verifyToken(token);
